89e5cedaf704d6f81124647513ccdb17cab288ee
Bruno Gonçalves <128926281+BGoncalvess@users.noreply.github.com>
2024-02-29 16:25:38 +0000

    Update prepareCodeTest.groovy

--- vars/prepareCodeTest.groovy
+++ vars/prepareCodeTest.groovy
@@ -15,19 +15,3 @@ def call(String tags) {
 
     def newTag = "${tagParts[0]}.${tagParts[1]}.${tagParts[2]}"
     echo "New tag: ${newTag}"
-
-    // Now, create and push the new tag to GitHub
-    withCredentials([usernamePassword(credentialsId: 'f22bc348-cc9c-4455-aab5-e33412be7604', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_TOKEN')]) {
-        // Get the latest commit's author name and email
-        def authorName = sh(script: 'git log -1 --pretty=%an', returnStdout: true).trim()
-        def authorEmail = sh(script: 'git log -1 --pretty=%ae', returnStdout: true).trim()
-
-        sh """
-            git config --local user.name "${authorName}"
-            git config --local user.email "${authorEmail}"
-            
-            git tag -a ${newTag} -m "Updated tag ${newTag}"
-            git push https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/BGoncalvess/agv-do-bruno.git --tags
-        """
-    }
-}
