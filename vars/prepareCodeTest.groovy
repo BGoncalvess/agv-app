3fc5602a94077e046b1cd98c12db3d0d2a90b200
Bruno Gonçalves <Goncalves02Bruno@gmail.com>
2024-03-07 14:43:58 +0000

    Changed credentials in vars preparetag code

--- vars/prepareCodeTest.groovy
+++ vars/prepareCodeTest.groovy
@@ -15,3 +15,16 @@ def call(String tags) {
 
     def newTag = "${tagParts[0]}.${tagParts[1]}.${tagParts[2]}"
     echo "New tag: ${newTag}"
+
+    withCredentials([usernamePassword(credentialsId: '884e60f4-2593-46b7-8fc0-b8745791ce4a', usernameVariable: 'GIT_USERNAME', passwordVariable: 'GIT_TOKEN')]) {
+        def authorName = sh(script: 'git log -1 --pretty=%an', returnStdout: true).trim()
+        def authorEmail = sh(script: 'git log -1 --pretty=%ae', returnStdout: true).trim()
+
+        sh """
+            git config --local user.name "${authorName}"
+            git config --local user.email "${authorEmail}"
+            
+            git tag -a ${newTag} -m "Updated tag ${newTag}"
+            git push https://${GIT_USERNAME}:${GIT_TOKEN}@github.com/BGoncalvess/agv-app.git --tags
+        """
+    }
